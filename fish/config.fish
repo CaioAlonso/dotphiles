039216c3d18bb75bd03b5194c1c95183cd50e343
Caio Alonso <caio@caioalonso.com>
2018-01-11 21:01:13 -0200

    Fish

---
+++ fish/config.fish
@@ -0,0 +1,18 @@
+set PATH /home/caio/bin/ $PATH
+
+set fish_greeting
+
+alias dutotal 'du -cksh'
+alias l 'ls'
+alias ls 'ls --color -F'
+alias la 'ls -la'
+alias lat 'la -t'
+alias ga 'git add'
+alias gc 'git commit'
+alias gd 'git diff'
+alias gs 'git status'
+alias tmux 'TERM xterm-256color tmux'
+alias ta 'tmux attach'
+alias vim nvim
+alias servidor 'mosh --ssh="ssh -p 26" caioalonso'
+alias update 'sudo pacman -Syu'
