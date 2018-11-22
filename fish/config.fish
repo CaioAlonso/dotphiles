6653a55f18045d48f3bb935d90958dad38eb3c44
Caio Alonso <caio@caioalonso.com>
2018-11-22 12:32:47 -0200

    Fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,17 +1,27 @@
-set PATH /home/caio/bin/ /usr/lib/emscripten/ $PATH
+set PATH /home/caio/bin/ /home/caio/.gem/ruby/2.5.0/bin $PATH
 
 set fish_greeting
 
-alias dutotal 'du -cksh'
-alias l 'ls'
-alias ls 'ls --color -F'
-alias la 'ls -la'
-alias lat 'la -t'
-alias ga 'git add'
-alias gc 'git commit'
-alias gd 'git diff'
-alias gs 'git status'
-alias ta 'tmux attach'
-alias vim nvim
-alias servidor 'mosh --ssh="ssh -p 26" caioalonso'
-alias update 'sudo pacman -Syu'
+alias l='ls --literal --color=auto --group-directories-first'
+alias la='l -l --human-readable --all'
+
+alias g='git'
+alias gs='git status'
+alias ga='git add'
+alias gc='git commit'
+alias gcm='git commit -m'
+alias gd='git diff'
+
+alias p='pacaur'
+alias tmux='tmux -2'
+alias ta='tmux attach'
+alias mkd='mkdir -pv'
+
+alias listenmoe='mpv https://listen.moe/opus'
+
+# Start X at login
+if status --is-login
+  if test -z "$DISPLAY" -a $XDG_VTNR = 1
+    exec startx
+  end
+end
