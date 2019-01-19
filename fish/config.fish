d736f437c598e5f45e401981bff165e2170920ef
Caio Alonso <caio@caioalonso.com>
2019-01-19 12:51:31 -0200

    Fish

---
+++ fish/config.fish
@@ -0,0 +1,15 @@
+set PATH ~/bin ~/go/bin $PATH
+set fish_greeting
+
+abbr l 'ls --literal --color=auto --group-directories-first'
+abbr la 'l -l --human-readable --all'
+abbr g 'git'
+abbr gs 'git status'
+abbr ga 'git add'
+abbr gc 'git commit'
+abbr gcm 'git commit -m'
+abbr gd 'git diff'
+abbr p 'yay'
+abbr tmux 'tmux -2'
+abbr ta 'tmux attach'
+abbr mkd 'mkdir -pv'
